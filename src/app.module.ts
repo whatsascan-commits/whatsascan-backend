52a325b4ddd7d86d01284e295a3ca76bf3039d4a
Hafiz Hamza <hafiz@myco.io>
2026-05-13 17:10:31 +0500

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { SessionModule } from './session/session.module';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
@@ -20,11 +20,9 @@ import { SocketGateway } from './gateway/socket.gateway';
     // MONGODB
     // =========================
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URL'),
+      useFactory: () => ({
+        uri: process.env.MONGO_URL,
       }),
-      inject: [ConfigService],
     }),
 
     // =========================
